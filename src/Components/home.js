e655b417d9723cbb6369c3451c1cae7633aaa43f
Obaid <obaidkhan317@gmail.com>
2022-04-15 00:45:57 +0500

    commit

--- src/Components/home.js
+++ src/Components/home.js
@@ -7,15 +7,15 @@ function Home() {
     <div className='hero-container' id='home'>
     
     <div className='text-home-cont-three'>
-<p className='text-home-three'>Have you ever heard about <span className='span-home'>Nabdidh</span>?</p>
+<p className='text-home-three'>Have you ever heard about <span className='span-home'>Nabidh</span>?</p>
 
         </div>
         <div className='text-home-cont-one'>
-<p className='text-home-one'> Have You filled <span className='span-home'>Nabdidh Survey</span>?</p>
+<p className='text-home-one'> Have You filled <span className='span-home'>Nabidh Survey</span>?</p>
 
         </div>
         <div className='text-home-cont-two'>
-<p className='text-home-two'> Is your EMR <span className='span-home'>Nabdidh compliant</span>?</p>
+<p className='text-home-two'> Is your EMR <span className='span-home'>Nabidh compliant</span>?</p>
 
         </div>
        
@@ -26,7 +26,7 @@ function Home() {
         <div className='lost-cont'>
 <p className='lost-text'> Lost <span className='span-home'>?</span></p>
 <div>
-  <p className='lost-here'>We are here to <span className='span-a'>assist</span> you on any question related to <span className='span-a'>nabidh</span>.</p>
+  <p className='lost-here'>We are here to <span className='span-a'>assist</span> you on any question related to <span className='span-a'>Nabidh</span>.</p>
 </div>
         </div>
  
